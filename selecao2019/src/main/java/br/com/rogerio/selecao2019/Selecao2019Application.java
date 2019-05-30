075057dc651cf7318d74e2f1c21777dd3e88685d
Rogerio Feitoza <rogerio.feitoza@basis.com.br>
2019-05-30 09:15:40 -0300

    Arquivos restantes

---
+++ selecao2019/src/main/java/br/com/rogerio/selecao2019/Selecao2019Application.java
@@ -0,0 +1,13 @@
+package br.com.rogerio.selecao2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Selecao2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Selecao2019Application.class, args);
+	}
+
+}
